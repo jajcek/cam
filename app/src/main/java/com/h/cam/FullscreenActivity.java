fff322bf434ff60dada894fcb0bcd9a6476e14df
h <jajcek.30@gmail.com>
2019-01-11 23:38:03 +0100

    prototype of drawing rectangle on preview

--- app/src/main/java/com/h/cam/FullscreenActivity.java
+++ app/src/main/java/com/h/cam/FullscreenActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -59,6 +62,41 @@ public class FullscreenActivity extends Activity {
 
     private void runPreview() {
         preview = new Preview((SurfaceView) findViewById(R.id.previewSurface));
+
+        SurfaceView a = (SurfaceView) findViewById(R.id.hintSurface);
+
+
+        SurfaceHolder h = a.getHolder();
+        h.setFormat(PixelFormat.TRANSPARENT);
+        h.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                paint.setTextSize(12);
+                paint.setColor(Color.RED);
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setStrokeWidth(5);
+
+                Path p = new Path();
+                p.moveTo(0, 0);
+                Canvas c = holder.lockCanvas();
+                c.save();
+                p.lineTo(200, 200);
+                c.drawRect(0, 0, 200, 200, paint);
+                c.restore();
+                holder.unlockCanvasAndPost(c);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
 
         prepareButtons(preview.getCamera());
     }
